9e78e92677b093b2dfcde2634c07c2fbae359976
Josh Davis <josh@joshldavis.com>
2014-07-06 22:37:51 -0500

    Use init.zsh over multiple bundles in .zshrc

---
+++ init.zsh
@@ -0,0 +1,12 @@
+source ./aliases/aliases.zsh
+source ./aliases/latex.zsh
+
+source ./custom/load.zsh
+source ./custom/z.zsh
+
+source ./env/env.zsh
+
+source ./functions/functions.zsh
+source ./functions/tmux.zsh
+
+source ./path/paths.zsh
