ddfb073e4ba272f215dc99111b63ae03d96c0f28
Josh Davis <josh@joshldavis.com>
2014-09-27 06:05:49 -0700

    Add source for coloring script like all good ppl

--- colors/config.zsh
+++ colors/config.zsh
@@ -1,3 +1,5 @@
+# Add coloring to man pages
+# Source: https://wiki.archlinux.org/index.php/man_page#Colored_man_pages
 man() {
     env LESS_TERMCAP_mb=$'\E[01;31m' \
     LESS_TERMCAP_md=$'\E[01;38;5;74m' \
