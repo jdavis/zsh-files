3153e632190040ce1bd992c7fd7c1d4f7c35d02f
Josh Davis <jdavis@users.noreply.github.com>
2020-12-14 21:55:03 -0800

    Remove lib/grep.zsh from init
    Whoops

--- init.zsh
+++ init.zsh
@@ -3,7 +3,6 @@ SOURCE=${0%/*}
 # Lib
 source $SOURCE/lib/completion.zsh
 source $SOURCE/lib/git.zsh
-source $SOURCE/lib/grep.zsh
 source $SOURCE/lib/history.zsh
 source $SOURCE/lib/key-bindings.zsh
 source $SOURCE/lib/misc.zsh
